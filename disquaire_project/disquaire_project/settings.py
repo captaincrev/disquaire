acde2ac71c7c7f8e43bdf12408c4eb4194668950
captaincrev <36740237+captaincrev@users.noreply.github.com>
2020-07-01 17:02:29 +0200

    50%

--- disquaire_project/disquaire_project/settings.py
+++ disquaire_project/disquaire_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6%$ljojoiv!jh-p1hnscz_0d6tin6wj)1j^(w-ey9ylb5og9cg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
